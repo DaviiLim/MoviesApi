8b0b9d4133705eab6601876c039c2daad96ae8af
Lobo <davilobo004@gmail.com>
2026-05-29 09:13:06 -0300

    Resolvendo Warning em items.

--- Domain/DTOs/Pagination/PaginationResponse.cs
+++ Domain/DTOs/Pagination/PaginationResponse.cs
@@ -2,7 +2,7 @@
 {
     public class PaginationResponse<T>
     {
-        public IEnumerable<T> Items { get; set; }
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
         public int TotalItems { get; set; }
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
